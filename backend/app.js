50441b5b3c3fc3594233141bbb5243274a731204
DiegoCarballo <diegohoraciocarballo@gmail.com>
2023-02-28 12:05:00 -0300

    Se crea un servidor

---
+++ backend/app.js
@@ -0,0 +1,41 @@
+﻿'use strict'
+// cargar modulos de node para crear el servidor
+
+var express = require('express');
+var bodyParser = require ('body-parser');
+
+// Ejecutar express(http)
+
+var app = express();
+
+
+//Cargar ficheros rutas
+
+// Middlewares
+
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+//CORS
+
+
+//Añadir prefijos a rutas
+
+//ruta o metodo de prueba para el API REST
+
+app.get('/probando', function(req, res){
+    console.log("Hola Mundo")
+    return res.status(200).send({
+            curso: 'Master en FrameWork JS',
+           autor: 'Victor Robles',
+            URL: 'VictorRoblesWeb.es'
+    }
+  
+    
+    )
+   
+});
+
+// exportar el modulo (fichero actual)
+module.exports = app;
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,19 @@
 ﻿'use strict'
 
 var mongoose =require('mongoose');
+var app = require('./app');
 
-mongoose.con
+var port = 3700;
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://127.0.0.1:27017/api_rest_blog', {useNewUrlParser: true})
+    .then ( () => {
+        
+            console.log("servidor corriendo correctamente !!!");
+            
+            app.listen(port, ()=>{
+            console.log("servidor corriendo correctamente en la url: 127.0.0.1:"+port)
+            })
+        
+        });
+ 
